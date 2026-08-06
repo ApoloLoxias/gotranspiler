708798c6597b98ee5ed26f04f82fa08c64c597c4
ApoloLoxias <apololoxias@gmail.com>
2026-08-06 10:00:50 -0300

    identifying next token of  arithmetic expression

--- ari_parser.go
+++ ari_parser.go
@@ -1,41 +1,60 @@
 package main
 
-import "slices"
 import "fmt"
+import "unicode"
 
 func Parse(e Expression, s []rune) (Expression, []rune) {
 	return e, s
 }
 
-func NextToken(s []rune) []rune {
-	nextToken := make([]rune, 0)
-
-	for i := 0; i < len(s); i++ {
-		if slices.Contains([]rune("0123456789"), s[i]) {
-			nextToken = append(nextToken, s[i])
-		} else {
-			break
+func IsPlusMinusSlashOrAsterisk(s rune) bool {
+	for _, r := range []rune("+-/*") {
+		if s == r {
+			return true
 		}
 	}
-
-	return nextToken
+	return false
 }
 
-func main() {
-	s := []rune("123aw+7\n")
-	fmt.Println(string(NextToken(s)))
-	fmt.Println(string(NextTokenAlt(s)))
-}
-
-func NextTokenAlt(s []rune) []rune {
+func NextToken(s []rune) ([]rune, error) {
 	nextToken := make([]rune, 0)
+	var parsing rune
 
 	for {
+		if len(s) == 0 {
+			return nextToken, nil
+		}
 		nextToken = append(nextToken, s[0])
 		s = s[1:]
 
-		if slices.Contains([]rune(" \n()[]{}'\"`´~^\\|,.<>;:/?!@#$%¨&*=+"), s[0]) {
-			return nextToken
+		parsing = nextToken[len(nextToken)-1]
+		if unicode.IsDigit(parsing) {
+			for {
+				if len(s) == 0 || !unicode.IsDigit(s[0]) {
+					return nextToken, nil
+				}
+				nextToken = append(nextToken, s[0])
+				s = s[1:]
+				parsing = nextToken[len(nextToken)-1]
+			}
 		}
+
+		if IsPlusMinusSlashOrAsterisk(parsing) {
+			return nextToken, nil
+		}
+
+		if unicode.IsSpace(parsing) {
+			return nextToken, nil
+		}
+
+		return nil, fmt.Errorf("can't parse %q", parsing)
 	}
+}
+
+func main() {
+	twelve, _ := NextToken([]rune("12+2"))
+	onetwo, _ := NextToken([]rune("12 2 3"))
+	plus, _ := NextToken([]rune("+-"))
+	_, err := NextToken([]rune("a"))
+	fmt.Println(string(twelve), string(onetwo), string(plus), err)
 }
