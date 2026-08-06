45be25f55a6a998c58b08a96e2ea602a62422702
ApoloLoxias <apololoxias@gmail.com>
2026-08-06 08:54:37 -0300

    another parsing strategy

--- ari_parser.go
+++ ari_parser.go
@@ -22,6 +22,20 @@ func NextToken(s []rune) []rune {
 }
 
 func main() {
-	s := []rune("123aw")
+	s := []rune("123aw+7\n")
 	fmt.Println(string(NextToken(s)))
+	fmt.Println(string(NextTokenAlt(s)))
+}
+
+func NextTokenAlt(s []rune) []rune {
+	nextToken := make([]rune, 0)
+
+	for {
+		nextToken = append(nextToken, s[0])
+		s = s[1:]
+
+		if slices.Contains([]rune(" \n()[]{}'\"`´~^\\|,.<>;:/?!@#$%¨&*=+"), s[0]) {
+			return nextToken
+		}
+	}
 }
