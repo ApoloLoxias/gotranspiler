d2358a3d212e234089131aaaad775466c69c7524
ApoloLoxias <apololoxias@gmail.com>
2026-09-15 09:49:33 -0300

    Ok. This is a multi-line commit message
    TODO: in no particular order
    1. Application right or left associativity, order of operations and priority compared to arithmetics
    2. More exaustive testings
    3. Better testing
    4. Lispy printer for AST
    5. Clean up the code/make it more comprehensible. Perhaps stablish and enforce style guidelines

--- ast/parser.go
+++ ast/parser.go
@@ -2,18 +2,19 @@ package ast
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 
 	"github.com/ApoloLoxias/gotranspiler/lex"
 )
+
+/* --------EXPORTS---------- */
 
 func Parse(tokens []lex.Token) Expression {
 	parser := parser{tokens, nil, 0}
 	return parser.parse(0)
 }
 
-/* ------------------------- */
+/* ----MAIN PARSING LOGIC--- */
 
 type parser struct {
 	in  []lex.Token
@@ -21,119 +22,7 @@ type parser struct {
 	at  int
 }
 
-/*
-func (p *parser) parse(previousPriority int) Expression {
-
-	// Parse First
-	argToken := p.current()
-	var arg Expression
-	var fun Expression
-
-	if argToken.IsPrefix() {
-		priority := lex.PrefixPriority[argToken.Kind]
-		if p.next() == errEOF { // since arg is nill at this arm, could probably delete this, and delete p.next() from elif and else arms and just if p.next() == errEOF{return arg} out of the conditional, assuming, of course that p.next() on the else branch does indeed execute for all of the Terminals //I can't actually do that
-			return nil
-		}
-
-		prefixArg := p.parse(priority) //Note: p.next() is called inside the nested p.parse call
-		arg = ApplicationE{Function: Neg, Argument: prefixArg}
-
-		// guard clause
-		if p.at >= len(p.in) { //guard clause is necessary because p.next() within a nested p.parse call can't guarantee the safety of p.at/p.current() without
-			return arg
-		}
-	} else if argToken.IsFunction() {
-		switch argToken.Kind {
-		case lex.TokenCROSS_FUNC:
-			fun = Sum
-		}
-		if p.next() == errEOF {
-			return fun
-		}
-		arg = p.parse(0)
-		arg = ApplicationE{Function: fun, Argument: arg}
-		if p.next() == errEOF {
-			return arg
-		}
-	} else {
-		switch argToken.Kind {
-		case lex.TokenNUMBER:
-			value, _ := strconv.Atoi(argToken.Value)
-			arg = IntE{Value: value}
-		case lex.TokenOPEN_PARENTHESIS: //TODO
-			if p.next() == errEOF {
-				return nil
-			}
-			return p.parse(0)
-		}
-
-		if p.next() == errEOF { //executes on case lex.TokenNumber for now, but left out of switch because it will probably be a default step for Terminals (i.e. non-prefix, non-function, non-parenthesis, non-EOF tokens)
-			return arg
-		}
-	}
-
-	//parseFollowUp
-	if p.current().IsInfix() || p.current().IsParenthesis() {
-		for p.at < len(p.in) {
-			operator := p.current()
-
-			var operation Expression
-			switch operator.Kind {
-			case lex.TokenCROSS:
-				operation = Sum
-			case lex.TokenHYPHEN:
-				operation = Sub
-			case lex.TokenASTERISK:
-				operation = Mul
-			case lex.TokenFORWARD_SLASH:
-				operation = Div
-			case lex.TokenCLOSE_PARENTHESIS: //TODO
-				p.next()
-				return arg
-			}
-
-			priority := lex.InfixPriority[operator.Kind]
-			if priority <= previousPriority {
-				return arg
-			}
-
-			if p.next() == errEOF {
-				return ApplicationE{Function: operation, Argument: arg}
-			}
-
-			arg2 := p.parse(priority)
-
-			arg = ApplicationE{
-				Function: ApplicationE{Function: operation, Argument: arg},
-				Argument: arg2,
-			}
-		}
-
-		return arg
-	} else { //If dealing with "Expression1 Expression2" (will likely be preceded by else if p.current().IsPostfix())
-		// arg is Expression1, now we must apply Expression1(Expression2), by means of fun = (E1=arg); arg = E2; return fun(arg) (or assign fun(arg) to arg, which is the deafult return value
-		fun = arg
-		arg = p.parse(0)
-		return ApplicationE{Function: fun, Argument: arg} //maybe just assign it to arg and let arg be returned at the tail by default
-	}
-
-	return arg //currently unreachable except for prefix followed by EOF, which won't likely be a valid expression anyway // seems its not reachable even in that case now, huh!?
-}
-*/
-
-/*
-func (p *parser) parse(previousPriority int) Expression {
-	first := p.parseFirst()
-	//intE{1}
-
-	if p.at >= len(p.in) {
-		return first
-	}
-
-	return p.parseFollowUp(first, previousPriority)
-}
-*/
-
+// The First token of an expression is special because there is no token before it
 func (p *parser) parseFirst() Expression {
 	first := p.current()
 
@@ -169,18 +58,15 @@ func (p *parser) parseFirst() Expression {
 		return ApplicationE{Function: fun, Argument: arg}
 	}
 
-	fmt.Println("first=", first, " and is of kind ", first.Kind, "We are in a switch, and if it is open_parenthesus, we'll go to the breaking change!")
 	switch first.Kind {
 	case lex.TokenNUMBER:
 		value, _ := strconv.Atoi(first.Value)
 		p.next()
 		return IntE{value}
 	case lex.TokenOPEN_PARENTHESIS:
-		fmt.Println("First is '(' we we'll advance and check for EOF")
 		if p.next() == errEOF {
 			return nil
 		}
-		fmt.Println("Now at p.at=", p.at, " and p.current()=", p.current(), " will return toReturn:=p.parseFirst(), This is the breaking line!!!!!!!!")
 		toReturn := p.parseFirst() // An expression that follows "(" is a standalone expression and its first token must be a First. This type of call was why I separated parseFirst() into its own subfunction in the first place, so I should not call p.parse(0) here!
 		// p.parseFirst() breaks "( 1 ) + - 2 * ( 8 \n )", but p.parse() breaks ""(1+0-(42/1)+1-(0))""
 		if p.at < len(p.in) && p.current().Kind == lex.TokenCLOSE_PARENTHESIS { // toReturn is p.parseFirst(), because what follows "(" is a first, so we skip the infix loop, but than we don't really handle ")" in pareFirst() as we do in parse(0), so we handle it here
@@ -193,69 +79,24 @@ func (p *parser) parseFirst() Expression {
 			 */
 			p.next()
 		}
-		fmt.Println("returning toReturn:=p.parseFirst()=", toReturn)
 		return toReturn
 	}
 
-	return nil
+	return nil // Falls through if First is not a Prefix, a Function, a Number, nor an TokenOPEN_PARENTHESIS -> In other words, if First is not a valid First token (i.e. an infix)
+	// I guess such a fall-through could be used to assign different behaviour to non-first tokens in first position, i.e. let infixes be functionalized in first position even if they are not followed by $
 }
-
-/*
-func (p *parser) parseFollowUp(first Expression, previousPriority int) Expression {
-	//	var result Expression
-	var current lex.Token
-	for p.at < len(p.in) {
-		current = p.current()
-		if current.IsInfix() || current.IsParenthesis() {
-			for p.at < len(p.in) {
-				if current.Kind == lex.TokenCLOSE_PARENTHESIS {
-					p.next()
-					return first
-				}
-				var operation Expression
-				switch current.Kind {
-				case lex.TokenCROSS:
-					operation = Sum
-				case lex.TokenHYPHEN:
-					operation = Sub
-				case lex.TokenASTERISK:
-					operation = Mul
-				case lex.TokenFORWARD_SLASH:
-					operation = Div
-				}
-				priority := lex.InfixPriority[current.Kind]
-				if priority <= previousPriority {
-					return first
-				}
-				if p.next() == errEOF {
-					return ApplicationE{Function: operation, Argument: first}
-				}
-				result = ApplicationE{
-					Function: ApplicationE{Function: operation, Argument: first},
-					Argument: p.parse(priority),
-				}
-			}
-			//return result
-		} else {
-			fun := first
-			arg := p.parseFirst()
-			return ApplicationE{Function: fun, Argument: arg}
-		}
-	}
-	return first
-}
-*/
 
 func (p *parser) parse(previousPriority int) Expression {
+	//First token must be a prefix or a standalone expression (or ´(´)
 	first := p.parseFirst()
 	if p.at >= len(p.in) {
 		return first
 	}
 
+	// Now we will parse the second term, i.e. the infix/sufix
 	var fun Expression
 	//	Opening parenthesis mustn't trigger the loop, lest we update priority and arg2 := p.parse(priority), accepting `(` as the argument
-	//	if p.current().IsInfix() || p.current().IsParenthesis() {
-	if p.current().IsInfix() || p.current().Kind == lex.TokenCLOSE_PARENTHESIS {
+	if p.current().IsInfix() || p.current().Kind == lex.TokenCLOSE_PARENTHESIS { //start infix loop. When implementing sufixes, may want to have an aditional arm for the if-else (i.e. if-else if-else)
 		for p.at < len(p.in) {
 			operator := p.current()
 
@@ -275,33 +116,36 @@ func (p *parser) parse(previousPriority int) Expression {
 			}
 
 			priority := lex.InfixPriority[operator.Kind]
-			if priority <= previousPriority {
+			if priority <= previousPriority { // <= for left-associativity; < for right-associativity
 				return first
 			}
 
-			if p.next() == errEOF {
+			if p.next() == errEOF { // No second operand, so sufix instead of infix: "first opeator". When implementing actual sufixes will probably not be inside this arm of the outer if-else, and leave this just for partial application of incomplete infix expressions/tokens that are ordinarily infixes in suffix position
 				return ApplicationE{Function: operation, Argument: first}
 			}
 
-			arg2 := p.parse(priority)
+			arg := p.parse(priority) // The second operand: "first operator arg"
 
 			first = ApplicationE{
 				Function: ApplicationE{Function: operation, Argument: first},
-				Argument: arg2,
+				Argument: arg,
 			}
 		}
 
 		return first
 	} else { //If dealing with "Expression1 Expression2" (will likely be preceded by else if p.current().IsPostfix())
-		// arg is Expression1, now we must apply Expression1(Expression2), by means of fun = (E1=arg); arg = E2; return fun(arg) (or assign fun(arg) to arg, which is the deafult return value
+		// arg is Expression1, now we must apply Expression1(Expression2), by means of fun = (E1=first); arg = E2; return fun(arg)
 		fun = first
-		first = p.parse(0)
-		return ApplicationE{Function: fun, Argument: first} //maybe just assign it to arg and let arg be returned at the tail by default
+		arg := p.parse(0)
+		return ApplicationE{Function: fun, Argument: arg} //maybe just assign it to a variable that is returned at the tail by default
 	}
 
-	return first //currently unreachable except for prefix followed by EOF, which won't likely be a valid expression anyway // seems its not reachable even in that case now, huh!?
+	return first //currently unreachable except for prefix followed by EOF, which won't likely be a valid expression anyway // compiler says it is not reachable at all,huh!? //Currently using a if-else (posibly an if-else if-else when dealing with suffixes) which returns on both if and else. Makes sense that this is unreachable// SO the thing about assigning a var on else arm and letting it be returned  at the tail would make sense when implementing suffixes if suffixes and applications have semantically simillar return values? // I hope that doesn't happen
 }
 
+/* --------HELPERS---------- */
+
+// Note: length check can't guarantee safety of the outer function on recursive calls
 func (p *parser) next() error {
 	p.at++
 	if p.at >= len(p.in) {
